d328f94650d09eaf92f3d6c06951125ee88ac1d7
Matthew Bates <matthew117@hotmail.co.uk>
2013-12-02 13:30:38 +0000

    Added a note about Tkinter Buttons

--- TkinterIO.py
+++ TkinterIO.py
@@ -1,12 +1,17 @@
 from Tkinter import *
 
+# Difficult to change behaviour based on which button is clicked as command has no reference to self
+def onClick():
+    print("Hello")
+
 root = Tk()
-#root.geometry("600x600+300+300")
+root.geometry("600x600+300+300")
 root.title("Minesweeper")
 
 for y in range(0, 8):
     for x in range(0, 8):
-        Button(root, text="({0},{1})".format(x,y)).grid(row=y, column=x)
+        b = Button(root, text="({0},{1})".format(x,y), command=onClick)
+        b.grid(row=y, column=x)
 
 root.mainloop()
 
